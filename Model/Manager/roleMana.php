d1bdc95d7f62e45e7ee086523562902627d71c9b
Sylvie-B <unsolo@hotmail.fr>
2021-05-10 16:48:00 +0200

    role manager U-D

--- Model/Manager/roleMana.php
+++ Model/Manager/roleMana.php
@@ -4,7 +4,7 @@
 class roleMana {
     private PDO $pdo;
 
-    public function __construt($pdo) {
+    public function __construct($pdo) {
         $this->pdo = $pdo;
     }
 
@@ -47,5 +47,32 @@ class roleMana {
         $role = $search->fetch();
         $role = new Role($role['id_rol'], $role['roleName']);
         return $role;
+    }
+
+    // updateRole
+
+    /**
+     * @param $id
+     * @param $new_role
+     */
+    public function updateRole ($id, $new_role){
+        $search = $this->pdo->prepare("UPDATE role SET roleName = :new_role WHERE id_rol = $id");
+        $search->bindValue(':id', $id);
+        $search->bindValue(':new_role', $new_role);
+        if($search->execute()){
+            echo "role mis à jour";
+        }
+    }
+
+    // delete role
+
+    /**
+     * @param $id
+     */
+    public function supprRole ($id) {
+        $search = $this->pdo->prepare("DELETE FROM role WHERE id_rol = $id");
+        if($search->execute()){
+            echo "role supprimé";
+        }
     }
 }
