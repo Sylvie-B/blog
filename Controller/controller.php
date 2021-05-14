2710807c4acf26b42b88e535ebeb24a1cb28299c
Sylvie-B <unsolo@hotmail.fr>
2021-05-14 14:35:48 +0200

    work on ctrl

--- Controller/controller.php
+++ Controller/controller.php
@@ -9,12 +9,13 @@ class controller {
     }
 
     // display the selected view
+
     /**
      * @param string $view
      * @param string $tab
      * @param array $ref
      */
-    public function displayView(string $view, string $tab, $ref = []) {
+    public function selectView(string $view, string $tab, array $ref = []) {
         // display header
         require_once $_SERVER['DOCUMENT_ROOT'] . '/View/partials/header.php';
         // display body
@@ -23,35 +24,4 @@ class controller {
         require_once $_SERVER['DOCUMENT_ROOT'] . '/View/partials/footer.php';
     }
 
-    public function checkForm () {
-        if(isset($_POST['title'], $_POST['art_text'])){
-            // get data
-            // remove tags
-            $title = strip_tags($_POST['title']);
-            $art_text = strip_tags($_POST['art_text']);
-            // todo get author fk
-            $author_fk = 1;
-
-            // send data to database
-            $sql = $this->pdo->prepare("
-                INSERT INTO 'article'('title', 'art_text', 'author_fk')
-                VALUES (:title, :art_text, :fk)");
-
-            $sql->bindValue(":title", $title);
-            $sql->bindValue(":art_text", $art_text);
-            $sql->bindValue(":fk", $author_fk);
-
-            if($sql->execute()){
-                echo "article ajouté";
-            }
-            else {
-                echo "erreur lors de l'ajout";
-            }
-        }
-        else {
-            echo "Veuillez remplir tous les champs";
-        }
-    }
 }
-
-
--- View/partials/footer.php
+++ View/partials/footer.php
@@ -1,7 +1,6 @@
 <!--    connexion / articleForm / articleView + comment  -->
             <footer>
                 <div id="olderTitle">
-                    <?= $ref[0] . " " . $ref[1] . " " . $ref[2] ?>
                 </div>
             </footer>
         </section>
